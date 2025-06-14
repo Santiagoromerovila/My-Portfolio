89fc72e972a668b20b6b8ba2fbdd78f75282067e
Santiagoromerovila <santiago.romero.vila@gmail.com>
2025-06-14 21:53:07 +0200

    Update language8.js

--- js/traducciones/proyectos/language8.js
+++ js/traducciones/proyectos/language8.js
@@ -71,7 +71,7 @@ document.addEventListener("DOMContentLoaded", () => {
       en: {
         title1:"Carwash",
         nave1:"Home",
-        nave2:"Curriculum",
+        nave2:"Resume",
         nave3:"Projects",
         nave4:"Jobs",
         nave5:"Press",
