ee5e329b42dc8a2a827be3beed4693d1f13a1aee
Santiagoromerovila <santiago.romero.vila@gmail.com>
2025-06-14 21:53:38 +0200

    Update language6.js

--- js/traducciones/proyectos/page/language6.js
+++ js/traducciones/proyectos/page/language6.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
       en: {
         title1:"Projects2",
         nave1:"Home",
-        nave2:"Curriculum",
+        nave2:"Resume",
         nave3:"Projects",
         nave4:"Jobs",
         nave5:"Press",
