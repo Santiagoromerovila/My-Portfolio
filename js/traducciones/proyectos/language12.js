32b6a1c12adc2d5daaf7b18497816db41a37f607
Santiagoromerovila <santiago.romero.vila@gmail.com>
2025-06-14 21:52:31 +0200

    Update language12.js

--- js/traducciones/proyectos/language12.js
+++ js/traducciones/proyectos/language12.js
@@ -47,7 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
       en: {
         title1:"Semantic Book Recommender Project",
         nave1:"Home",
-        nave2:"Curriculum",
+        nave2:"Resume",
         nave3:"Projects",
         nave4:"Jobs",
         nave5:"Press",
